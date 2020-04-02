64059886d694c54687193693b779668627188588
AlexLeko <lk.alexds@gmail.com>
2020-04-02 17:35:00 -0300

    Adicionando registro por Command Line Runner

--- src/main/kotlin/com/alexleko/pontointeligente/PontointeligenteApplication.kt
+++ src/main/kotlin/com/alexleko/pontointeligente/PontointeligenteApplication.kt
@@ -1,10 +1,49 @@
 package com.alexleko.pontointeligente
 
+import com.alexleko.pontointeligente.documents.Empresa
+import com.alexleko.pontointeligente.documents.Funcionario
+import com.alexleko.pontointeligente.enums.PerfilEnum
+import com.alexleko.pontointeligente.repositories.EmpresaRepository
+import com.alexleko.pontointeligente.repositories.FuncionarioRepository
+import com.alexleko.pontointeligente.utils.SenhaUtils
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PontointeligenteApplication
+class PontointeligenteApplication (val empresaRepository: EmpresaRepository,
+								   val funcionarioRepository: FuncionarioRepository) : CommandLineRunner {
+
+	override fun run(vararg args: String?) {
+
+		empresaRepository.deleteAll()
+		funcionarioRepository.deleteAll()
+
+		val empresa: Empresa = Empresa("Empresa", "10443887000146")
+		empresaRepository.save(empresa)
+
+		val admin: Funcionario = Funcionario("Admin",
+				"admin@empresa.com",
+				SenhaUtils().gerarBCript("123456"),
+				"25708317000", PerfilEnum.ROLE_ADMIN, empresa.id!!)
+		funcionarioRepository.save(admin)
+
+		System.out.println("Teste" + admin.id)
+
+		val funcionario: Funcionario = Funcionario("Funcionario",
+				"funcionario@empresa.com",
+				SenhaUtils().gerarBCript("123456"),
+				"44325441557", PerfilEnum.ROLE_USUARIO, empresa.id!!)
+		funcionarioRepository.save(funcionario)
+		System.out.println("Teste" + funcionario.id)
+
+
+		println("Empresa ID: ${empresa.id}")
+		println("Admin ID: ${admin.id}")
+		println("Funcionario ID: ${funcionario.id}")
+
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<PontointeligenteApplication>(*args)
--- src/main/kotlin/com/alexleko/pontointeligente/dtos/CadastroPFDto.kt
+++ src/main/kotlin/com/alexleko/pontointeligente/dtos/CadastroPFDto.kt
@@ -1,7 +1,6 @@
 package com.alexleko.pontointeligente.dtos
 
 import org.hibernate.validator.constraints.Length
-import org.hibernate.validator.constraints.br.CNPJ
 import org.hibernate.validator.constraints.br.CPF
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotEmpty
